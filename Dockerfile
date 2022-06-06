674b184037f49e9d5af4d8bd32cd40d8d17f6c9c
Frederick Morlock <frederickgeek8@gmail.com>
2022-06-06 12:48:19 -0700

    Had to do some *magic*

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,15 @@ FROM nvcr.io/nvidia/pytorch:21.04-py3
 LABEL Name=paperspace Version=0.0.1
 RUN mkdir -p /setup
 COPY . /setup
+
+# Install deps
+RUN pip install -U pip
+RUN pip install -r /setup/requirements.txt
+
+# set up python-apt
+RUN sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
+RUN apt-get update && apt-get install -y python3 python3-apt
+
 RUN /setup/install_deps.sh
 COPY ssh_colab.ipynb /workspace
 CMD ["bash", "-c", "/setup/startup.sh"]
--- install_deps.sh
+++ install_deps.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
-pip install -U pip
-pip install -r /setup/requirements.txt
+# install colab_ssh deps
+export DEBIAN_FRONTEND=noninteractive
+
+ln -fs /usr/share/zoneinfo/America/Pacific /etc/localtime
+apt-get install -y tzdata
+dpkg-reconfigure --frontend noninteractive tzdata
+
+pwd
+cd /usr/lib/python3/dist-packages/
+ln -s apt_inst.cpython-38-x86_64-linux-gnu.so apt_inst.so
+ln -s apt_pkg.cpython-38-x86_64-linux-gnu.so apt_pkg.so
+
+apt-get source python3-distutils-extra
+cd python-distutils-extra-*
+python setup.py install
+
+apt-get source python3-apt
+cd python-apt-2.0.0ubuntu0.20.04.7
+apt build-dep -y ./
+python setup.py install
